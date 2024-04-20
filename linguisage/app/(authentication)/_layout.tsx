be5694f4b39d83e30c9b439d09d9970df50e35d6
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-04-20 10:28:02 +0300

    Initial commit

---
+++ linguisage/app/(authentication)/_layout.tsx
@@ -0,0 +1,9 @@
+import { Link, Tabs } from 'expo-router'
+import {Pressable, SafeAreaView} from 'react-native'
+import {Text, View} from 'tamagui'
+
+export default function TabLayout() {
+  return (
+    <SafeAreaView><Text>Authentic</Text></SafeAreaView>
+  )
+}
