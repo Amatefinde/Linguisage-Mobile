24b0951113c3c63e6a4434efeb20914274270065
Alexandr Gurov <MyNameIIIypuk@yandex.ru>
2024-04-29 16:07:32 +0300

    remove unused imports

--- linguisage/app/(tabs)/settings.tsx
+++ linguisage/app/(tabs)/settings.tsx
@@ -1,8 +1,5 @@
 import { Text, View, ScrollView, YStack, Paragraph } from "tamagui";
-import { RefreshControl, SafeAreaView, ScrollViewComponent } from "react-native";
-import LastLiterature from "../../components/LastLiterature";
-import WordCards from "../../components/WordCards";
-import { useState } from "react";
+";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import UserCard from "../../components/Settings/UserCard";
 export default function SettingsScreen() {
